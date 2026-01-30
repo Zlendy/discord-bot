baca3b0d650ea78f5957a6289a809c34f0ae0032
Zlendy <zlendy@proton.me>
2026-01-30 19:40:33 +0100

    feat: Implement `/rename` command

--- commands.go
+++ commands.go
@@ -1,6 +1,10 @@
 package main
 
-import "github.com/bwmarrin/discordgo"
+import (
+	"fmt"
+
+	"github.com/bwmarrin/discordgo"
+)
 
 type CommandHandler struct {
 	Command discordgo.ApplicationCommand
@@ -54,6 +58,78 @@ var commands = map[string]*CommandHandler{
 				Type: discordgo.InteractionResponseChannelMessageWithSource,
 				Data: &discordgo.InteractionResponseData{
 					Flags:   discordgo.MessageFlagsEphemeral,
+					Content: response,
+				},
+			})
+		},
+	},
+
+	"rename": {
+		Command: discordgo.ApplicationCommand{
+			Name: "rename",
+			NameLocalizations: &map[discordgo.Locale]string{
+				discordgo.SpanishES: "renombrar",
+			},
+			Description: "Change another user's name",
+			DescriptionLocalizations: &map[discordgo.Locale]string{
+				discordgo.SpanishES: "Cambiar el nombre de otro usuario",
+			},
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Type: discordgo.ApplicationCommandOptionUser,
+					Name: "user",
+					NameLocalizations: map[discordgo.Locale]string{
+						discordgo.SpanishES: "usuario",
+					},
+					Description: "User",
+					DescriptionLocalizations: map[discordgo.Locale]string{
+						discordgo.SpanishES: "Usuario",
+					},
+					Required: true,
+				},
+				{
+					Type: discordgo.ApplicationCommandOptionString,
+					Name: "name",
+					NameLocalizations: map[discordgo.Locale]string{
+						discordgo.SpanishES: "nombre",
+					},
+					Description: "Name",
+					DescriptionLocalizations: map[discordgo.Locale]string{
+						discordgo.SpanishES: "Nombre",
+					},
+					Required: true,
+				},
+			},
+		},
+		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			user := i.ApplicationCommandData().GetOption("user")
+			name := i.ApplicationCommandData().GetOption("name")
+
+			userId := user.UserValue(s).ID
+
+			member, err := s.GuildMember(i.GuildID, userId)
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+
+			err = s.GuildMemberNickname(i.GuildID, userId, name.StringValue())
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+
+			var response string
+			switch i.Locale {
+			case discordgo.SpanishES:
+				response = fmt.Sprintf("El nombre de `%s` ha sido cambiado a `%s`", member.Nick, name.StringValue())
+			default:
+				response = fmt.Sprintf("`%s`'s name has been changed to `%s`", member.Nick, name.StringValue())
+			}
+
+			s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
 					Content: response,
 				},
 			})
