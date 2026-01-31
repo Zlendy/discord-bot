ce0e777ff4d52f75d4cf90abb1060536325c62e5
Zlendy <zlendy@proton.me>
2026-01-31 01:52:06 +0100

    feat: Implement `/join` and `/leave` commands

--- commands.go
+++ commands.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/bwmarrin/discordgo"
@@ -135,6 +136,100 @@ var commands = map[string]*CommandHandler{
 			})
 		},
 	},
+
+	"join": {
+		Command: discordgo.ApplicationCommand{
+			Name: "join",
+			NameLocalizations: &map[discordgo.Locale]string{
+				discordgo.SpanishES: "unirse",
+			},
+			Description: "Join your current voice channel",
+			DescriptionLocalizations: &map[discordgo.Locale]string{
+				discordgo.SpanishES: "Unirse a tu canal de voz actual",
+			},
+		},
+		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			voice, err := findUserVoiceState(s, i.Member.User.ID)
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+
+			_, err = s.ChannelVoiceJoin(i.GuildID, voice.ChannelID, false, false)
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+
+			var response string
+			switch i.Locale {
+			case discordgo.SpanishES:
+				response = "Me he unido a tu canal de voz"
+			default:
+				response = "I joined your voice channel"
+			}
+
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Flags:   discordgo.MessageFlagsEphemeral,
+					Content: response,
+				},
+			})
+
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+		},
+	},
+
+	"leave": {
+		Command: discordgo.ApplicationCommand{
+			Name: "leave",
+			NameLocalizations: &map[discordgo.Locale]string{
+				discordgo.SpanishES: "salirse",
+			},
+			Description: "Leave a voice channel",
+			DescriptionLocalizations: &map[discordgo.Locale]string{
+				discordgo.SpanishES: "Salirse de un canal de voz",
+			},
+		},
+		Handler: func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+			voice, ok := s.VoiceConnections[i.GuildID]
+			if !ok {
+				messageError(s, i)
+				return
+			}
+
+			err := voice.Disconnect()
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+
+			var response string
+			switch i.Locale {
+			case discordgo.SpanishES:
+				response = "Me he salido del canal de voz"
+			default:
+				response = "I left the voice channel"
+			}
+
+			err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+				Type: discordgo.InteractionResponseChannelMessageWithSource,
+				Data: &discordgo.InteractionResponseData{
+					Flags:   discordgo.MessageFlagsEphemeral,
+					Content: response,
+				},
+			})
+
+			if err != nil {
+				messageError(s, i)
+				return
+			}
+		},
+	},
 }
 
 func messageError(s *discordgo.Session, i *discordgo.InteractionCreate) {
@@ -153,4 +248,15 @@ func messageError(s *discordgo.Session, i *discordgo.InteractionCreate) {
 			Content: response,
 		},
 	})
+}
+
+func findUserVoiceState(s *discordgo.Session, userId string) (*discordgo.VoiceState, error) {
+	for _, guild := range s.State.Guilds {
+		for _, voice := range guild.VoiceStates {
+			if voice.UserID == userId {
+				return voice, nil
+			}
+		}
+	}
+	return nil, errors.New("Could not find user's voice state")
 }
